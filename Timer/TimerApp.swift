635b05f1d485c7931a519e71b0e1499dc8eed7c9
Abdul Khan <akhan@lcs.on.ca>
2020-11-02 11:24:25 -0500

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Abdul Ahad Khan on 2020-11-02.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
